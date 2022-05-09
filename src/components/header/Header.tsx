37b9d37b4c62bc1e1ab1336504d4c32b232fdeeb
Max Zadarnovskiy <maxzadarnovskiyy@gmail.com>
2022-05-09 21:49:36 +0930

    Basic landing slide (hero) done

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,21 +6,21 @@ export default function Header() {
     <div className="header header-fixed u-unselectable header-animated">
       <div className="header-brand">
         <div className="nav-item no-hover">
-          <a href='/'><h6 className="title">Max Zadarnovskiy</h6></a>
+          <a href='/'><h6 className="title">MZ</h6></a>
+        </div>
+        <div className="nav-item text-center">
+          <a href="https://github.com/Borismeister" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={['fab', 'github']} />
+          </a>
+        </div>
+        <div className="nav-item text-center">
+          <a href="https://www.linkedin.com/in/mzadarnovskiy/" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={['fab', 'linkedin']} />
+          </a>
         </div>
       </div>
       <div className='header-nav'>
         <div className="nav-left">
-          <div className="nav-item text-center">
-            <a href="https://github.com/Borismeister" target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={['fab', 'github']} />
-            </a>
-          </div>
-          <div className="nav-item text-center">
-            <a href="https://www.linkedin.com/in/mzadarnovskiy/" target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={['fab', 'linkedin']} />
-            </a>
-          </div>
           <div className="nav-item has-sub toggle-hover" id="dropdown">
             <a href='/#' className="nav-dropdown-link">Projects</a>
             <ul className="dropdown-menu dropdown-animated" role="menu">
---
+++ src/components/landing_hero/LandingHero.tsx
@@ -0,0 +1,14 @@
+
+export default function LandingHero() {
+  return (
+    <div className="hero fullscreen bg-purple-500">
+      <div className="hero-body">
+        <div className="content">
+          <h1 className="title text-white">Hi, I'm Max</h1>
+          <h3 className="subtitle">a web developer</h3>
+          <a href="#contact" className="btn outline btn--pilled btn-light">Contact Me!</a>
+        </div>
+      </div>
+    </div>
+  )
+}
